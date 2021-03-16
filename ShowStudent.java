aa0368c54ad9d0e72ebcf0c38621e2943e40b211
Kevin-Salva <salvaj501@gmail.com>
2021-03-16 15:22:05 +0800

    showstudent1

---
+++ ShowStudent.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class ShowStudent {
+    public static void main(String[] args)
+    {
+
+//Create student object
+        Student student1 = new Student();
+//Set student parameters
+        student1.setCredits(4);
+        student1.setID(1);
+        student1.setPoints(4);
+//Calculate GPA and assign to variable
+        double gradepoint = student1.calGPA(student1.getPoints(), student1.getCredits());
+
+//Print results
+        System.out.println("Student ID: " + student1.getID());
+        System.out.println("Number of credits: " + student1.getCredits());
+        System.out.println("Number of points: " + student1.getPoints());
+        System.out.println("GPA: " + gradepoint);
+
+
+    }//End main method
+
+}//end class
+
