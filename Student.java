754d0d307bc911786514c69380a9d68aa8a2106f
Kevin-Salva <salvaj501@gmail.com>
2021-03-16 15:21:37 +0800

    student,java

---
+++ Student.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Student {
+    private int StudID;
+    int credits;
+    int points;
+    double gpa;
+
+    public void setID (int ID){
+        ID = StudID;
+    }
+    public int getID(){
+        return StudID;
+    }
+    public void setCredits(int credit){
+        credits=credit;
+    }
+    public int getCredits(){
+        return credits;
+    }
+    public void setPoints(int point){
+        points=point;
+    }
+    public double getPoints(){
+        return points;
+    }
+    public double calGPA(double gradepoint){
+        gradepoint = points/credits;
+        return gradepoint;
+    }
+    public void printMessage(){
+        System.out.println("Credits: "+credits);
+        System.out.println("Points: "+points);
+        System.out.println("The GPA is: "+gpa);
+    }
+
+}
